af47f3d8fb05350b1bbfceb4c778d7b3499d8fa4
Alexey <103638723+incw@users.noreply.github.com>
2023-03-08 03:09:32 +0700

    create Project Water Balance

---
+++ app/src/main/java/ru/smolyakoff/waterbalance/presentation/PersonalDataFragment.kt
@@ -0,0 +1,23 @@
+package ru.smolyakoff.waterbalance.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.smolyakoff.waterbalance.R
+
+
+class PersonalDataFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_personal_data, container, false)
+    }
+
+
+}
